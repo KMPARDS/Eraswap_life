4c2beda08c662453c0fb16ff91af263d007bbc26
SauravKanchan <sauravnk30@gmail.com>
2019-07-25 13:37:49 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<svelte:component this={$page.component} {...$page.props} />
+<script>
+  import router, { page } from './router'
+
+  router.start()
+</script>
---
+++ src/router.js
@@ -0,0 +1,21 @@
+import { writable } from 'svelte/store'
+import router from 'page'
+
+export const page = writable({
+  component: null,
+  props: {}
+})
+
+router('/', () =>
+  import(/* webpackChunkName: "index" */ './views/Index.svelte').then(module =>
+    page.set({ component: module.default })
+  )
+)
+
+router('/something', () =>
+  import(/* webpackChunkName: "something" */ './views/Something.svelte').then(
+    module => page.set({ component: module.default })
+  )
+)
+
+export default router
