aa86a1bd24e32d14b48cf568eccac32486ea8651
SauravKanchan <sauravnk30@gmail.com>
2019-07-27 15:57:05 +0530

    Add mnemonic check

--- src/views/AccessMyWallet.svelte
+++ src/views/AccessMyWallet.svelte
@@ -1,20 +1,35 @@
 <script>
     import Navbar from './NavBar.svelte'
     let mnemonic="";
+    let error_message="";
     function load_wallet() {
-        let provider = new ethers.providers.InfuraProvider("kovan");
-        let wallet = ethers.Wallet.fromMnemonic(mnemonic);
-        window.wallet = new ethers.Wallet(wallet.privateKey, provider)
+        try{
+            let provider = new ethers.providers.InfuraProvider("kovan");
+            let wallet = ethers.Wallet.fromMnemonic(mnemonic);
+            window.wallet = new ethers.Wallet(wallet.privateKey, provider)
+            document.getElementById("dashboard").click()
+        }catch (e) {
+            error_message = "Invalid Mnemonic";
+        }
     }
 </script>
 
 <Navbar title="Dashboard"/>
 <div class="container">
+{#if error_message != ""}
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  {error_message}
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+{/if}
 <div class="row">
 <div class="col">
 <h4>Access through mnemonic</h4>
 <textarea bind:value={mnemonic}></textarea>
-<a href="/dashboard" on:click={load_wallet}>Access</a>
+<button on:click={load_wallet}>Access my wallet</button>
+<a href="/dashboard" id="dashboard" style="display: none">Access</a>
 </div>
 </div>
 </div>
