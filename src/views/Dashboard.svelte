e1f981dc8677a10d9a4c8adb2b4a7d1a62a6fb1a
SauravKanchan <sauravnk30@gmail.com>
2019-07-27 18:44:32 +0530

    one app login

--- src/views/Dashboard.svelte
+++ src/views/Dashboard.svelte
@@ -2,19 +2,52 @@
     import Navbar from './NavBar.svelte'
     let balance = "-";
     let address = "-";
+    let error_message = "";
+    let website = "";
     (async () => {
-        balance = String(await ethers.utils.formatEther(String(await wallet.getBalance())));
-        address = await wallet.getAddress();
-    })()
+        try{
+            balance = String(await ethers.utils.formatEther(String(await wallet.getBalance())));
+            address = await wallet.getAddress();
+        }catch (e) {
+            error_message = 'Wallet not loaded. Please Load your wallet '
+        }
+    })();
+
+    window.addEventListener('message', function(event) {
+        if(event.origin === "http://localhost:8000"){
+            if(website){
+                website.postMessage(wallet.privateKey,"*");
+            }else{
+                console.log("website not loaded")
+            }
+        }
+    });
+
+    async function load_website(event) {
+        let url = event.srcElement.attributes.data.value;
+    	website = await window.open( url, this);
+    }
 </script>
 
 <Navbar title="Access My Wallet"/>
 <div class="container">
+{#if error_message != ""}
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  {error_message}
+  <a href="/access-my-wallet">Here</a>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+{/if}
 <div class="row">
 <div class="col-6">
 ETH Balance - {balance}
 <br>
 Address - {address}
 </div>
+</div>
+<div class="row">
+<button class="col-4" data="http://localhost:8000/login" on:click={load_website}>Computeex</button>
 </div>
 </div>
--- src/views/NavBar.svelte
+++ src/views/NavBar.svelte
@@ -1,3 +1,6 @@
+<script>
+    let wallet;
+</script>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="/">EraSwap Shop</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,7 +10,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/">Home</a>
+        <a class="nav-link" href="/dashboard">Dashboard</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/create-new-wallet">Create New Wallet</a>
