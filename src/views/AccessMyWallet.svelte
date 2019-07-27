75065b0ccf8ad5f05e6c2504a825b10f572d53b5
SauravKanchan <sauravnk30@gmail.com>
2019-07-27 15:32:14 +0530

    Access my wallet with mnemonic

--- src/views/AccessMyWallet.svelte
+++ src/views/AccessMyWallet.svelte
@@ -1,5 +1,18 @@
 <script>
     import Navbar from './NavBar.svelte'
+    let mnemonic="";
+    function load_wallet() {
+        window.wallet = ethers.Wallet.fromMnemonic(mnemonic)
+    }
 </script>
 
 <Navbar title="Dashboard"/>
+<div class="container">
+<div class="row">
+<div class="col">
+<h4>Access through mnemonic</h4>
+<textarea bind:value={mnemonic}></textarea>
+<a href="/dashboard" on:click={load_wallet}>Access</a>
+</div>
+</div>
+</div>
