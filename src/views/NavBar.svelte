b642dd1f4fda6fdf15f4a24fd8faa3fc243bbcfc
SauravKanchan <sauravnk30@gmail.com>
2019-08-08 17:00:08 +0530

    Add logout

--- src/views/NavBar.svelte
+++ src/views/NavBar.svelte
@@ -1,9 +1,13 @@
 <script>
-    let wallet;
+    function logout(){
+        console.log("logout")
+        wallet=undefined;
+        document.getElementById("home").click()
+    }
 </script>
 
 <nav class="navbar navbar-expand-lg navbar-light">
-  <a class="navbar-brand" href="/"><img src="/images/es_newlogo-white-min.png"></a>
+  <a class="navbar-brand" href="/" id="home"><img src="/images/es_newlogo-white-min.png"></a>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -11,15 +15,17 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto navbar-right">
-      <!-- <li class="nav-item active" style="margin-right: 8px;">
-        <a class="nav-link text-white tm-button tm-button-lg tm-button-white tm-button-transparent" href="/dashboard">Dashboard</a>
-      </li> -->
       <li class="nav-item" style="margin-right: 8px; margin-bottom:10px">
         <a class="nav-link text-white tm-button tm-button-lg tm-button-white tm-button-transparent" href="/create-new-wallet" >Create New Wallet</a>
       </li>
       <li class="nav-item" style="margin-right: 8px; margin-bottom:10px">
         <a class="nav-link text-white tm-button tm-button-lg tm-button-white tm-button-transparent" href="/access-my-wallet" >Access My Wallet</a>
       </li>
+      {#if wallet}
+      <if class="nav-item" style="margin-right: 8px; margin-bottom:10px">
+        <a class="nav-link text-white tm-button tm-button-lg tm-button-white tm-button-transparent" href="#" on:click={logout}>Logout</a>
+      </if>
+      {/if}
     </ul>
   </div>
 </nav>
