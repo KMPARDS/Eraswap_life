2ab3ed339b917332772105fb77bfe33546a9e9e3
zemse <sohamzemse143@gmail.com>
2019-12-26 16:48:17 +0530

    remove curtain on chunk scripts run

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <svelte:component this={$page.component} {...$page.props} />
 <script>
   import router, { page } from './router'
-
   router.start()
+  document.getElementById('start-loader').style.display = 'none';
 </script>
--- src/main.js
+++ src/main.js
@@ -7,11 +7,11 @@ const app = new App({
 window.app = app
 
 // one app login
-window.onload = function(){
-  // !window.opener || window.opener.postMessage("loaded","*");
-
-  document.getElementById('start-loader').style.display = 'none';
-
-}
+// window.onload = function(){
+//   // !window.opener || window.opener.postMessage("loaded","*");
+//
+//   document.getElementById('start-loader').style.display = 'none';
+//
+// }
 
 export default app
