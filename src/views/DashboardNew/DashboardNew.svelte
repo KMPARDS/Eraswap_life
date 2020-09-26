ba2e71ef027a993649cbc93d68c0980e9f6932e5
zemse <sohamzemse143@gmail.com>
2020-09-26 20:02:50 +0530

    Fix text color

--- src/views/DashboardNew/DashboardNew.svelte
+++ src/views/DashboardNew/DashboardNew.svelte
@@ -709,7 +709,7 @@
    .address {
    margin:10px;
    background-color: #fff;
-   color: #fff232b2;
+   color: #691111;
    text-align: center;
    border-radius: 4px;
    /* margin: 0px 30px !important; */
