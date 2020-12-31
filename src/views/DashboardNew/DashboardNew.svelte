37e5a6cfc7199fcaa8810a7e61f0c2bbe478ac1e
Pranjal Agrawal <2018kucp1127@iiitkota.ac.in>
2020-12-31 13:40:15 +0530

    Update DashboardNew.svelte

--- src/views/DashboardNew/DashboardNew.svelte
+++ src/views/DashboardNew/DashboardNew.svelte
@@ -1410,6 +1410,13 @@ Submit
             </li>
             <li style="cursor:pointer;">
                <img
+                  src="images/ESCloudLogo.png"
+                  data="https://surveysdapp.com/"
+                  on:click={load_website}
+                  alt="" />
+            </li>
+            <li style="cursor:pointer;">
+               <img
                   src="images/dashboardNew/value OF Farmers.jpg"
                   data="https://valueoffarmers.org/"
                   on:click={load_website}
